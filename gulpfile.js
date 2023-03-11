98035a2bcd4509a3d03fa4b34d0f0ec632e91e96
ayana <ayana.s.prose@gmail.com>
2023-03-11 21:44:10 +0900

    submission

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 // gulpプラグインの読み込み
-const gulp = require("gulp");
+const gulp = require('gulp');
 // Sassをコンパイルするプラグインの読み込み
-const sass = require("gulp-sass");
-// style.scssをタスクを作成する
-gulp.task("default", function() {
-  // style.scssファイルを取得
-  return (
-  gulp
-  .src("sass/style.scss")
+const sass = require('gulp-sass')(require('sass'));
+
+// style.scssの監視タスクを作成する
+gulp.task('default', function () {
+  // ★ style.scssファイルを監視
+  return gulp.watch('sass/**/*.scss', function () {
+  // style.scssの更新があった場合の処理
+
+  // style.scssファイルを取得 （全てのSCSSファイルを取得）
+  return gulp.src('sass/**/*.scss')
   // Sassのコンパイルを実⾏
-  .pipe(sass())
+  .pipe(sass({
+    outputStyle: 'expanded'
+  })
+  // Sassのコンパイルエラーを表⽰
+  // (これがないと⾃動的に⽌まってしまう)
+  .on('error', sass.logError))
   // cssフォルダー以下に保存
-  .pipe(gulp.dest("css"))
-  );
+  .pipe(gulp.dest('css','css2'));
+  });
 });
